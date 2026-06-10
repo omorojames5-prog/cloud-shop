02e206631c8490944b18852bab271b4777537a2a
james omoro <omorojames5@gmail.com>
2026-06-10 20:35:20 +0300

    added home page

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
-import { CartProvider } from "./context/CartContext";
+import CartProvider from "./context/CartContext";  
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
